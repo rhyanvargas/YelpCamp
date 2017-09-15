890ec45fd2bc242f01e190e16523bcb09d8a9fc8
Rhyan Vargas <rhyanv1@gmail.com>
2017-09-15 01:13:00 -0400

    connect mongodb and add mongoose functions to save new campgrounds to database

--- app.js
+++ app.js
@@ -1,43 +1,69 @@
-// UTILITIES
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+// ========> UTILITIES <=========
+var express = require('express')
+  , app = express()
+  , bodyParser = require('body-parser')
+  , mongoose = require('mongoose')
 
-
+mongoose.connect('mongodb://localhost/yelp_camp');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-var campgrounds = [
-      { name: 'Dark Hollow Falls', image: 'https://i.pinimg.com/600x315/73/f6/52/73f6528c2cc62cfcf05cd71575bb0224.jpg'},
-      { name: 'Billy Goat Trail', image: 'http://www.livedogrow.com/wp-content/uploads/view-of-river-from-billy-goat-trail.jpg'},
-      { name: 'Gunpowder Falls', image: 'https://i1.wp.com/recreationnews.com/wp-content/uploads/2016/02/5347709848_e6095825a8_b.jpg?resize=678%2C381'}
-];
+// var campgrounds = [
+//       { name: 'Dark Hollow Falls', image: 'https://i.pinimg.com/600x315/73/f6/52/73f6528c2cc62cfcf05cd71575bb0224.jpg'},
+//       { name: 'Billy Goat Trail', image: 'http://www.livedogrow.com/wp-content/uploads/view-of-river-from-billy-goat-trail.jpg'},
+//       { name: 'Gunpowder Falls', image: 'https://i1.wp.com/recreationnews.com/wp-content/uploads/2016/02/5347709848_e6095825a8_b.jpg?resize=678%2C381'}
+// ];
 
-// ROUTES
+// ========> SCHEMA SETUP <=========
+
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+var Campground = mongoose.model('Campground', campgroundSchema);
+
+//      
+// ========> ROUTES <=========
+
 app.get('/', function(req,res){
   res.render('landing');
 });
 
 app.get('/campgrounds', function(req,res){
-  res.render('campgrounds', {campgrounds: campgrounds});
+  // Get campgrounds from database
+  Campground.find({}, function(err, allCampgrounds){
+    if (err) {
+      console.log('ERROR!');
+      console.log(err);
+    } else {
+      res.render('campgrounds', {campgrounds: allCampgrounds});
+    }
+  }); 
 });
 
 app.post('/campgrounds', function(req,res){
   // get data from form and add to campgrounds array
-  var name = req.body.name;
-  var image = req.body.image;
-  var newCampground = {name: name, image: image};
+  var campName = req.body.formName;
+  var campImage = req.body.formImage;
+  var newCampground = { name: campName, image: campImage}
 
-  campgrounds.unshift(newCampground);
-  // redirect back to campgrounds
-  res.redirect('/campgrounds');
+  // Create new campground and save to campgrounds database
+  Campground.create(newCampground, function(err, newlyCreated){
+    if (err) {
+      console.log(err);
+    } else {
+      // redirect back to campgrounds
+      res.redirect('/campgrounds');
+    }
+  });
 });
 
 app.get('/campgrounds/new', function(req,res){
   res.render('new');
-});
+}); 
 
-// SERVER
+// ========> SERVER <=========
 app.listen(8080, function(){
   console.log('YELPCAMP SERVER STARTED! --> http://localhost:8080');
 })
